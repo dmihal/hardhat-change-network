b5f5106d556d8cbc8d745f48cdb689fafe150f38
David Mihal <david@dmihal.com>
2021-06-22 17:31:42 +0100

    Add getProvider

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { extendEnvironment } from "hardhat/config";
 import { createProvider } from "hardhat/internal/core/providers/construction";
+  import { EthereumProvider } from "hardhat/types/provider";
 
 // This import is needed to let the TypeScript compiler know that it should include your type
 // extensions in your npm package's types file.
@@ -10,19 +11,32 @@ extendEnvironment((hre) => {
   // We add a field to the Hardhat Runtime Environment here.
   // We use lazyObject to avoid initializing things until they are actually
   // needed.
+  const providers: { [name: string]: EthereumProvider } = {};
+
+  hre.getProvider = function getProvider(name: string): EthereumProvider {
+    if (!providers[name]) {
+      providers[name] = createProvider(
+        name,
+        this.config.networks[name],
+        this.config.paths,
+        this.artifacts,
+      );
+    }
+    return providers[name];
+  }
+
   hre.changeNetwork = function changeNetwork(newNetwork: string) {
     if (!this.config.networks[newNetwork]) {
       throw new Error(`changeNetwork: Couldn't find network '${newNetwork}'`);
     }
 
+    if (!providers[this.network.name]) {
+      providers[this.network.name] = this.network.provider;
+    }
+
     this.network.name = newNetwork;
     this.network.config = this.config.networks[newNetwork];
-    this.network.provider = createProvider(
-      newNetwork,
-      this.config.networks[newNetwork],
-      this.config.paths,
-      this.artifacts,
-    );
+    this.network.provider = this.getProvider(newNetwork);
 
     if ((this as any).ethers) {
       const { EthersProviderWrapper } = require("@nomiclabs/hardhat-ethers/internal/ethers-provider-wrapper");
--- src/type-extensions.ts
+++ src/type-extensions.ts
@@ -1,14 +1,15 @@
 // If your plugin extends types from another plugin, you should import the plugin here.
 
 // To extend one of Hardhat's types, you need to import the module where it has been defined, and redeclare it.
-import "hardhat/types/config";
 import "hardhat/types/runtime";
-
+import { EthereumProvider } from "hardhat/types/provider";
 
 declare module "hardhat/types/runtime" {
+
   // This is an example of an extension to the Hardhat Runtime Environment.
   // This new field will be available in tasks' actions, scripts, and tests.
   export interface HardhatRuntimeEnvironment {
     changeNetwork(newNetwork: string): void;
+    getProvider(newNetwork: string): EthereumProvider;
   }
 }
